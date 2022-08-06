64957e084cdb056de0fa9151d97270bbe5c774fe
alaqsaka <aqsaabidin15@gmail.com>
2022-08-06 14:42:48 +0700

    create menu, add scrollbar for menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,91 @@
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+`;
+const Wrapper = styled.div``;
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+          <h1>tes</h1>
+        </Wrapper>
+      </Main>
+    </Container>
   );
 }
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,189 @@
+import React from "react";
+import styled from "styled-components";
+import Yutub from "../img/logo.png";
+import { Home } from "@mui/icons-material";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+
+const Container = styled.div`
+  flex: 1;
+  background-color: #202020;
+  height: 100vh;
+  color: white;
+  position: sticky;
+  top: 0;
+  overflow: hidden;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    /* padding: 5px; */
+    width: 8px;
+    border-radius: 100vw;
+    /* border: 1px solid red; */
+    /* background-color: gray; */
+  }
+
+  &::-webkit-scrollbar-track {
+    border-radius: 100vw;
+    margin-top: 50px;
+    margin-bottom: 200px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    /* width: 20px; */
+    
+    background-color: #6b6969;
+    outline: 1px solid slategrey;
+    border-radius: 100vw;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    /* width: 20px; */
+    
+    background-color: #a09e9e;
+    outline: 1px solid slategrey;
+    border-radius: 15px;
+  }
+`;
+
+const Wrapper = styled.div`
+  padding: 18px 26px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
+
+const Img = styled.img`
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  gap: 20px;
+  padding: 7.5px 0px;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Login = styled.div``;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 300;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Menu = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={Yutub} />
+          Yutub
+        </Logo>
+        <Item>
+          <Home />
+          Home
+        </Item>
+        <Item>
+          <ExploreOutlinedIcon />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlinedIcon />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <VideoLibraryOutlinedIcon />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlinedIcon />
+          History
+        </Item>
+        <Login>Sign in to like videos, comment, and subscribe.</Login>
+        <Button>
+          {" "}
+          <AccountCircleOutlinedIcon />
+          SIGN IN
+        </Button>
+        <Hr />
+        <Item>
+          <LibraryMusicOutlinedIcon />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlinedIcon />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlinedIcon />
+          Gaming
+        </Item>
+        <Item>
+          <MovieOutlinedIcon />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlinedIcon />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlinedIcon />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsOutlinedIcon />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlinedIcon />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlinedIcon />
+          Help
+        </Item>
+        <Item>
+          <SettingsBrightnessOutlinedIcon />
+          Light Mode
+        </Item>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Menu;
